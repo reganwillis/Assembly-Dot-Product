fac3493d6005bc75475436a68bd100c230c08785
Regan Willis <reganwillis@outlook.com>
2024-09-30 12:13:49 -0400

    no multiplier (wip)

--- dot_product.s
+++ dot_product.s
@@ -2,15 +2,27 @@
 
 # NOTE: vectors should be hardcoded
 # NOTE: vectors must be equal lengths
-a = [1, 2, 3, 4, 5]
-b = [6, 7, 8, 9, 10]
-vector_length = 5
+a = [2, 2, 3, 4, 5]  # TODO: store in $s1
+b = [3, 7, 8, 9, 10]  # TODO: store in $s2
+addi $t0, $zero, 5  # vector length
 
-sum = $zero  # var to hold the dot product
-i = $zero  # create iterator var
+add $t1, $zero, $zero  # sum: holds the dot product
+add $t2, $zero, $zero  # i: dot product loop iterator
 
 # loop over every index of the vectors
-$t1 = mul(a[i], b[i])
-sum = sum + $t1
-++i
-if i < vector_length: goto line 13  # loop condition
+
+# load vectors at the index
+# TODO: compute offset
+lw $t3, $t2($s1)  # a[i] 2
+lw $t4, $t2($s2)  # b[i] 3
+
+# multiply vectors at i
+add $t5, $zero, $zero  # j: multiplier loop iterator
+add $t6, $zero, $zero  # product
+add $t6, $t6, $t3
+addi $t5, $t5, 1  # ++j
+blt $t4, $t5, L22  # b[i] < j
+
+add $t1, $t1, $t6  # add product to sum
+addi $t2, $t2, 1  # ++i
+blt $t2, $t0, L26  # i < vector length
